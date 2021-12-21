fa0cf6e52b53a1d8814a6d732d14d84220bd597c
Oguzhan Celik <ogzzcelik@gmail.com>
2021-12-21 15:24:06 +0300

    Created Pages files

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import BookStack from './BookStack';
 import {NavigationContainer} from '@react-navigation/native';
+import Library from '../pages/Library';
+import Settings from '../pages/Settings';
+import Search from '../pages/Search';
 
 const Tab = createBottomTabNavigator();
 
@@ -10,6 +13,9 @@ export default function Navigation() {
         <NavigationContainer>
             <Tab.Navigator screenOptions={{headerShown: false}}>
                 <Tab.Screen name="Book" component={BookStack} />
+                <Tab.Screen name="Search" component={Search} />
+                <Tab.Screen name="My Library" component={Library} />
+                <Tab.Screen name="Settings" component={Settings} />
             </Tab.Navigator>
         </NavigationContainer>
     )
