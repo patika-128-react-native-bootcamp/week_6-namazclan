b9666931deadc2d027f1f63d3c88020c591dce99
salik-a <salika@mef.edu.tr>
2021-12-22 18:49:54 +0300

    style update

--- src/components/Cards/ComicCard/ComicCard.js
+++ src/components/Cards/ComicCard/ComicCard.js
@@ -5,29 +5,22 @@ import { Shadow } from 'react-native-shadow-2';
 
 export default function ComicCard({ comic, onPress }) {
 
+    const source = comic.images[0] !== undefined ? `${comic.images[0].path}/portrait_incredible.jpg` : "https://legacycomics.com/wp-content/uploads/2019/07/marvel-comics-1000-allred-60s.png"
     return (
-        <TouchableWithoutFeedback onPress={onPress}>
+        <TouchableWithoutFeedback >
             <View style={styles.container}>
                 <Shadow >
-                    {comic.images[0] !== undefined ? (
-                        <Image
-                            style={styles.image}
-                            source={{ uri: `${comic.images[0].path}/portrait_incredible.jpg` }}
-                        />
-                    ) : (
-                        <Image
-                                style={styles.image}
-                                source={{ uri: "https://legacycomics.com/wp-content/uploads/2019/07/marvel-comics-1000-allred-60s.png" }}
-                            />
-
-                    )}
+                    <Image
+                        style={styles.image}
+                        source={{ uri: source }}
+                    />
                 </Shadow>
-                <TouchableOpacity style={styles.readContainer}>
+                <TouchableOpacity style={styles.readContainer} onPress={onPress}>
                     <Text style={styles.read}>READ NOW</Text>
                 </TouchableOpacity>
-                <View style={styles.titleContainer}>
-                    <Text style={styles.title} numberOfLines={5}>{comic.title.slice(0, 20)}...</Text>
-                </View>
+
+                <Text style={styles.title} numberOfLines={2}>{comic.title.slice(0, 20)}...</Text>
+
             </View>
         </TouchableWithoutFeedback>
     )
--- src/pages/Comic/ComicDetail/Layout/ComicDetailLayout.js
+++ src/pages/Comic/ComicDetail/Layout/ComicDetailLayout.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import {SafeAreaView, View, Text } from 'react-native'
+import { SafeAreaView, View, Text, ImageBackground } from 'react-native'
 import styles from './ComicDetailLayout.styles';
 import Header from '../../../../components/Cards/Header'
-export default function ComicDetailLayout({details, onPress}) {
+export default function ComicDetailLayout({ details, onPress }) {
+    const source = details.images[0] !== undefined ? `${details.images[0].path}/portrait_incredible.jpg` : "https://legacycomics.com/wp-content/uploads/2019/07/marvel-comics-1000-allred-60s.png"
     return (
-        <SafeAreaView>
-            <Header back onPress={onPress}/>
-            <Text>Comic Detail Layout</Text>
-            <Text>{details.title}</Text>
+        <SafeAreaView style={styles.container}>
+            <ImageBackground source={{ uri: source }} resizeMode="cover" style={styles.image}>
+                <View style={styles.innerContainer}>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.title}>{details.title}</Text>
+                    </View>
+                </View>
+            </ImageBackground>
         </SafeAreaView>
     )
 }
