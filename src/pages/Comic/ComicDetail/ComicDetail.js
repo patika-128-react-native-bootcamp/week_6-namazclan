0db4e4ccc42a6f5ad80779bd08c11f0bdf26481e
salik-a <salika@mef.edu.tr>
2021-12-24 02:32:06 +0300

    theme and language set functionality added

--- src/pages/Comic/ComicDetail/ComicDetail.js
+++ src/pages/Comic/ComicDetail/ComicDetail.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import{View, Text} from 'react-native'
 import ComicDetailLayout from './Layout'
 import { baseUrl, hash, apikey, ts } from "../../../Utils/constants";
 import useFetch from '../../../hooks/useFetch';
@@ -7,17 +8,30 @@ export default function ComicDetail({ route, navigation }) {
     const { loading, data, error } = useFetch(`${baseUrl}/comics/${route.params.id}/characters?&ts=${ts}&apikey=${apikey}&hash=${hash}`);
 
     
-
-    const response = data ? data.data : null
-
-    const responseData=response && response.results 
-    
-    console.log(responseData);
-    
+    //const response = data ? data.data.results : null
+    //const responseData=response && response.results 
+    //console.log(response);
     const handleBack = () => {
         navigation.goBack();
     }
+
+    if (loading) {
+        return (
+            <View>
+                <Text>Loading</Text>
+            </View>
+        )
+    }
+    if (error) {
+
+        return (
+            <View>
+                <Text>error</Text>
+            </View>
+        )
+    }
+
     return (
-        <ComicDetailLayout details={route.params} onPress={handleBack} data={response} charactersData={responseData} />
+        <ComicDetailLayout details={route.params} onPress={handleBack} charactersData={data.data.results} />
     )
 }
