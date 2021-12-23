bc30aa2432def8b340fc3454181c6ee0361f5134
salik-a <salika@mef.edu.tr>
2021-12-23 16:07:57 +0300

    mini update

--- src/pages/Search/Search.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import SearchLayout from './Layout/'
-
-export default function Search() {
-    return (
-        <SearchLayout/>
-    )
-}
